346d9fa2d9641ba8093fb484522f7561eceb107a
wesson2775 <862832617@qq.com>
2025-04-16 12:38:16 +0800

    1

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -5,8 +5,10 @@ module Jekyll
     safe true
 
     def generate(site)
-      all_tags = site.posts.map { |post| post.data['tags'] }.flatten.uniq
+      # 从所有文章中提取唯一标签
+      all_tags = site.posts.map { |post| post.data['tags'] }.flatten.uniq.compact
       all_tags.each do |tag|
+        # 为每个标签生成一个页面
         site.pages << TagPage.new(site, site.source, tag)
       end
     end
@@ -16,12 +18,12 @@ module Jekyll
     def initialize(site, base, tag)
       @site = site
       @base = base
-      @dir  = File.join('tags', tag)
+      @dir  = File.join('tags', tag.to_s)
       @name = 'index.html'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
-      self.data['tag'] = tag
+      self.data['tag'] = tag.to_s
       self.data['title'] = "标签: #{tag}"
     end
   end
